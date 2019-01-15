4651447bd2601fe737204471b82f6b58e99c306a
Huong Dang <huongd97@knights.ucf.edu>
2019-01-15 17:42:39 -0500

    feat: SQL files

---
+++ app/group54.sql
@@ -0,0 +1,130 @@
+CREATE DATABASE `BeArGuest`;
+
+USE `BeArGuest`;
+
+CREATE TABLE `Profile` (
+  `userID` VARCHAR(100),
+  `fName` VARCHAR(100) NOT NULL,
+  `lName` VARCHAR(100) NOT NULL,
+  `email` VARCHAR(255) NOT NULL UNIQUE,
+  `dateCreated` DATETIME NOT NULL,
+  `role` VARCHAR(50) NOT NULL,
+  `imageURL` TINYTEXT,
+  `userStatus` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`userID`)
+);
+
+CREATE TABLE `Category` (
+  `categoryID` INT NOT NULL AUTO_INCREMENT,
+  `categoryName` VARCHAR(100) NOT NULL UNIQUE,
+  PRIMARY KEY (`categoryID`)
+);
+
+CREATE TABLE `Park` (
+  `parkID` INT NOT NULL AUTO_INCREMENT,
+  `parkName` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`parkID`)
+);
+
+CREATE TABLE `MealType` (
+  `mealTypeID` INT NOT NULL AUTO_INCREMENT,
+  `mealTypeName` VARCHAR(100) NOT NULL UNIQUE,
+  PRIMARY KEY (`mealTypeID`)
+);
+
+CREATE TABLE `Day` (
+  `dayID` INT NOT NULL AUTO_INCREMENT,
+  `day` DATE NOT NULL UNIQUE,
+  PRIMARY KEY (`dayID`)
+);
+
+CREATE TABLE `RestaurantType` (
+  `restaurantTypeID` INT NOT NULL AUTO_INCREMENT,
+  `restaurantTypeName` VARCHAR(100) NOT NULL UNIQUE,
+  PRIMARY KEY (`restaurantTypeID`)
+);
+
+CREATE TABLE `Land` (
+  `landID` INT NOT NULL AUTO_INCREMENT,
+  `landName` VARCHAR(100) NOT NULL,
+  `parkID` INT NOT NULL,
+  PRIMARY KEY (`landID`),
+  FOREIGN KEY (`parkID`) REFERENCES `Park` (`parkID`)
+);
+
+
+CREATE TABLE `TripPlan` (
+  `tripID` INT NOT NULL AUTO_INCREMENT,
+  `userID` VARCHAR(100) NOT NULL,
+  `startDate` DATETIME NOT NULL,
+  `endDate` DATETIME NOT NULL,
+  PRIMARY KEY (`tripID`),
+  FOREIGN KEY (`userID`) REFERENCES `Profile` (`userID`)
+);
+
+CREATE TABLE `Restaurant` (
+  `restaurantID` INT NOT NULL AUTO_INCREMENT,
+  `landID` INT NOT NULL,
+  `restaurantTypeID` INT NOT NULL,
+  `restaurantName` VARCHAR(100) NOT NULL,
+  `restaurantStatus` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`restaurantID`),
+  FOREIGN KEY (`landID`) REFERENCES `Land` (`landID`),
+  FOREIGN KEY (`restaurantTypeID`) REFERENCES `RestaurantType` (`restaurantTypeID`)
+);
+
+
+CREATE TABLE `Item` (
+  `itemID` INT NOT NULL AUTO_INCREMENT,
+  `restaurantID` INT NOT NULL,
+  `itemName` VARCHAR(100) NOT NULL,
+  `itemDescription` TEXT,
+  `secret` BOOLEAN NOT NULL,
+  `vegan` BOOLEAN NOT NULL,
+  `substitution` TEXT,
+  `itemStatus` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`itemID`),
+  FOREIGN KEY (`restaurantID`) REFERENCES `Restaurant` (`restaurantID`)
+);
+
+CREATE TABLE `ItemInfo` (
+  `itemID` INT NOT NULL,
+  `categoryID` INT NOT NULL,
+  `mealTypeID` INT NOT NULL,
+  PRIMARY KEY (`itemID`, `categoryID`, `mealTypeID`),
+  FOREIGN KEY (`itemID`) REFERENCES `Item` (`itemID`),
+  FOREIGN KEY (`categoryID`) REFERENCES `Category` (`categoryID`),
+  FOREIGN KEY (`mealTypeID`) REFERENCES `MealType` (`mealTypeID`)
+);
+
+CREATE TABLE `Review` (
+  `userID` VARCHAR(100) NOT NULL,
+  `itemID` INT NOT NULL,
+  `comment` TEXT,
+  `rating` INT,
+  `isFavorite` BOOLEAN NOT NULL,
+  PRIMARY KEY (`userID`, `itemID`),
+  FOREIGN KEY (`userID`) REFERENCES `Profile` (`userID`),
+  FOREIGN KEY (`itemID`) REFERENCES `Item` (`itemID`)
+);
+
+CREATE TABLE `Bookmark` (
+  `userID` VARCHAR(100) NOT NULL,
+  `restaurantID` INT NOT NULL,
+  `dateBookmarked` DATETIME NOT NULL,
+  PRIMARY KEY (`restaurantID`, `userID`),
+  FOREIGN KEY (`userID`) REFERENCES `Profile` (`userID`),
+  FOREIGN KEY (`restaurantID`) REFERENCES `Restaurant` (`restaurantID`)
+);
+
+CREATE TABLE `MealPlan` (
+  `tripID` INT NOT NULL,
+  `restaurantID` INT NOT NULL,
+  `dayID` INT NOT NULL,
+  `mealTypeID` INT NOT NULL,
+  PRIMARY KEY (`tripID`, `restaurantID`, `dayID`, `mealTypeID`),
+  FOREIGN KEY (`tripID`) REFERENCES `TripPlan` (`tripID`),
+  FOREIGN KEY (`restaurantID`) REFERENCES `Restaurant` (`restaurantID`),
+  FOREIGN KEY (`dayID`) REFERENCES `Day` (`dayID`),
+  FOREIGN KEY (`mealTypeID`) REFERENCES `MealType` (`mealTypeID`)
+);
---
+++ app/insertData.sql
@@ -0,0 +1,102 @@
+USE `BeArGuest`;
+
+INSERT INTO
+`Profile` (`userID`, `fName`, `lName`, `email`, `dateCreated`, `role`, `imageURL`, `userStatus`)
+VALUES ('123456789', 'admin', 'admin', 'admin@admin.com', '18-11-01', 'admin', '#', 'ACTIVE'),
+       ('abcdefghi', 'user1', 'admin', 'user1@user.com', '18-11-01', 'user', '#', 'ACTIVE'),
+       ('987654321', 'user2', 'admin', 'user2@user.com', '18-11-01', 'user', '#', 'ACTIVE'),
+       ('111111111', 'user3', 'admin', 'user3@user.com', '18-11-01', 'user', '#', 'ACTIVE'),
+       ('222222222', 'user4', 'admin', 'user4@user.com', '18-11-01', 'user', '#', 'ACTIVE'),
+       ('333333333', 'user5', 'admin', 'user5@user.com', '18-11-01', 'user', '#', 'DELETED');
+
+INSERT INTO
+`Category` (`categoryName`)
+VALUES ('Daiya'),
+       ('Impossible'),
+       ('Sorbet'),
+       ('Beyond');
+
+INSERT INTO
+`Park` (`parkName`)
+VALUES ('Magic Kingdom Park'),
+       ('Epcot'),
+       ('Disney\'s Hollywood Studios'),
+       ('Disney\'s Animal Kingdom Theme Park'),
+       ('Disney\'s Typhoon Lagoon Water Park'),
+       ('Disney\'s Blizzard Beach Water Park');
+
+INSERT INTO
+`Land` (`landName`, `parkID`)
+VALUES ('Main Street, USA', (SELECT `parkID` FROM `Park` where `parkName` = 'Magic Kingdom Park')),
+       ('Adventureland', (SELECT `parkID` FROM `Park` where `parkName` = 'Magic Kingdom Park')),
+       ('Fantasyland', (SELECT `parkID` FROM `Park` where `parkName` = 'Magic Kingdom Park')),
+       ('Frontierland', (SELECT `parkID` FROM `Park` where `parkName` = 'Magic Kingdom Park')),
+       ('Liberty Square', (SELECT `parkID` FROM `Park` where `parkName` = 'Magic Kingdom Park')),
+       ('Tomorrowland', (SELECT `parkID` FROM `Park` where `parkName` = 'Magic Kingdom Park'));
+
+INSERT INTO
+`MealType` (`mealTypeName`)
+VALUES ('Breakfast'),
+       ('Lunch'),
+       ('Brunch'),
+       ('Dinner'),
+       ('Snack'),
+       ('Anything');
+
+INSERT INTO
+`RestaurantType` (`restaurantTypeName`)
+VALUES ('Quick Service'),
+       ('Table Service'),
+       ('Cart'),
+       ('Other');
+
+INSERT INTO
+`Day` (`day`)
+VALUES ('2018-10-01'),
+       ('2018-10-02'),
+       ('2018-10-03'),
+       ('2018-10-04'),
+       ('2018-10-05'),
+       ('2018-10-06'),
+       ('2018-10-07'),
+       ('2018-10-08'),
+       ('2018-10-09'),
+       ('2018-10-10'),
+       ('2018-10-11'),
+       ('2018-10-12'),
+       ('2018-10-13');
+
+INSERT INTO
+`Restaurant` (`landID`, `restaurantTypeID`, `restaurantName`, `restaurantStatus`)
+VALUES ((SELECT `landID` FROM `Land` WHERE `landName` = 'Main Street, USA' AND `parkID` = (SELECT `parkID` FROM `Park` WHERE `parkName` = 'Magic Kingdom Park')),
+        (SELECT `restaurantTypeID` FROM `RestaurantType` WHERE `restaurantTypeName` = 'Quick Service'), 'Casey\'s Corner', 'AVAILABLE');
+
+INSERT INTO
+`Restaurant` (`landID`, `restaurantTypeID`, `restaurantName`, `restaurantStatus`)
+VALUES ((SELECT `landID` FROM `Land` WHERE `landName` = 'Frontierland' AND `parkID` = (SELECT `parkID` FROM `Park` WHERE `parkName` = 'Magic Kingdom Park')),
+        (SELECT `restaurantTypeID` FROM `RestaurantType` WHERE `restaurantTypeName` = 'Quick Service'), 'Pecos Bill Tall Tale Cafe', 'AVAILABLE');
+
+INSERT INTO
+`Restaurant` (`landID`, `restaurantTypeID`, `restaurantName`, `restaurantStatus`)
+VALUES ((SELECT `landID` FROM `Land` WHERE `landName` = 'Liberty Square' AND `parkID` = (SELECT `parkID` FROM `Park` WHERE `parkName` = 'Magic Kingdom Park')),
+        (SELECT `restaurantTypeID` FROM `RestaurantType` WHERE `restaurantTypeName` = 'Quick Service'), 'Columbia Harbor House', 'AVAILABLE');
+
+INSERT INTO
+`Restaurant` (`landID`, `restaurantTypeID`, `restaurantName`, `restaurantStatus`)
+VALUES ((SELECT `landID` FROM `Land` WHERE `landName` = 'Fantasyland' AND `parkID` = (SELECT `parkID` FROM `Park` WHERE `parkName` = 'Magic Kingdom Park')),
+        (SELECT `restaurantTypeID` FROM `RestaurantType` WHERE `restaurantTypeName` = 'Quick Service'), 'Pinocchio Village Haus', 'AVAILABLE');
+
+INSERT INTO
+`Restaurant` (`landID`, `restaurantTypeID`, `restaurantName`, `restaurantStatus`)
+VALUES ((SELECT `landID` FROM `Land` WHERE `landName` = 'Fantasyland' AND `parkID` = (SELECT `parkID` FROM `Park` WHERE `parkName` = 'Magic Kingdom Park')),
+        (SELECT `restaurantTypeID` FROM `RestaurantType` WHERE `restaurantTypeName` = 'Quick Service'), 'The Friar\'s Nook', 'AVAILABLE');
+
+INSERT INTO
+`Restaurant` (`landID`, `restaurantTypeID`, `restaurantName`, `restaurantStatus`)
+VALUES ((SELECT `landID` FROM `Land` WHERE `landName` = 'Tomorrowland' AND `parkID` = (SELECT `parkID` FROM `Park` WHERE `parkName` = 'Magic Kingdom Park')),
+        (SELECT `restaurantTypeID` FROM `RestaurantType` WHERE `restaurantTypeName` = 'Quick Service'), 'Cosmic Ray\'s Starlight Café', 'AVAILABLE');
+
+INSERT INTO
+`Restaurant` (`landID`, `restaurantTypeID`, `restaurantName`, `restaurantStatus`)
+VALUES ((SELECT `landID` FROM `Land` WHERE `landName` = 'Tomorrowland' AND `parkID` = (SELECT `parkID` FROM `Park` WHERE `parkName` = 'Magic Kingdom Park')),
+        (SELECT `restaurantTypeID` FROM `RestaurantType` WHERE `restaurantTypeName` = 'Quick Service'), 'The Lunching Pad', 'AVAILABLE');
