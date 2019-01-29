126b84afc2260a4d653397ee73eb3a904237861e
Huong Dang <huongd97@knights.ucf.edu>
2019-01-29 18:07:50 -0500

    feat: models!

---
+++ app/model/ItemInfo.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router  = express.Router();
+var DB      = require('../db');
+var _       = require('lodash');
+
+
+// TODO: Code here
+
+module.exports = router;
