7f1b74f97443df10394fb9f3aaf1318cdcfc4d0b
jlaw <jacquelynlaw@gmail.com>
2019-02-01 15:06:39 -0500

    Restaurant Type route added, type in Restaurant.js file name - renamed

---
+++ app/model/Restaurant.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router  = express.Router();
+var DB      = require('../db');
+var _       = require('lodash');
+
+router.get('/getAll', async (req, res) => {
+    try {
+        const query  = `Select * from Restaurant;`;
+        const result = await DB.runQuery(query);
+        res.json(result);
+    } catch (e) {
+        console.log('Error getting all restaurant info:', e);
+        res.status(500).send('Check server logs for more info');
+    }
+});
+
+// bearguest.com/restaurants/getAllItems need restaurantID
+// req = { body: { name: 'Casey's Corner', restaurantID: 2 } }
+router.post('/getAllItems', async (req, res) => {
+    const {restaurantID} = req.body;
+    if (_.isNil(restaurantID)) {
+        res.status(401).send('Bad request');
+    } else {
+        try {
+            const query  = `select * from Items as I where I.restaurantID = ${escape(restaurantID)};`;
+            const result = await DB.runQuery(query);
+            res.json(result);
+        } catch (e) {
+            console.log('An error occurred when querying the database', e);
+            res.status(500).send('Check server logs for errors.');
+        }
+    }
+});
+
+// /restaurants/
+
+module.exports = router;
