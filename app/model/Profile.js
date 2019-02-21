29c539e7ca825bbd8e2726c5168abcbfdf4465d7
Huong Dang <huongd97@knights.ucf.edu>
2019-02-21 10:24:13 -0500

    feat: Add more CRUD operations to profile table

--- app/model/Profile.js
+++ app/model/Profile.js
@@ -19,6 +19,8 @@ const ROLE_OPTIONS   = {
     ADMIN: 'admin'
 };
 
+const ROLE_OPTIONS_VALUES = ['\'user\'', '\'admin\''];
+
 /**
  * Add a new profile to
  */
@@ -39,9 +41,8 @@ router.post('/create', async (req, res) => {
                   imageURL    = sqlstring.escape(userImageURL),
                   userID      = sqlstring.escape(uid);
 
-            const query = `INSERT INTO Profile (fName, lName, email, role, userID, dateCreated, userStatus, imageURL) 
+            const query  = `INSERT INTO Profile (fName, lName, email, role, userID, dateCreated, userStatus, imageURL) 
         VALUES (${fName}, ${lName}, ${email}, ${role}, ${userID}, ${dateCreated}, ${status}, ${imageURL})`;
-            console.log('query is', query);
             const result = await DB.runQuery(query);
             res.json({success: true});
         } catch (e) {
@@ -49,6 +50,90 @@ router.post('/create', async (req, res) => {
             res.json({success: false});
         }
     }
+});
+
+router.post('/getProfileById', async (req, res) => {
+    const {uid} = req.body;
+    if (!uid) {
+        return res.status(500).json({
+                                        error: `Cannot get profile because no ID was provided.`,
+                                    });
+    } else {
+        try {
+            const userID = sqlstring.escape(uid);
+            const query  = `SELECT * FROM Profile WHERE userID = ${userID}`;
+            const result = await DB.runQuery(query);
+            res.json(result);
+        } catch (e) {
+            console.log('Error retrieving profile from the database:', e);
+            res.status(500).json({
+                                     error: `Cannot retrieve profile from the database.`,
+                                 });
+        }
+    }
+});
+
+router.post('/isAdmin', async (req, res) => {
+    const {uid} = req.body;
+    if (!uid) {
+        return res.send(false);
+    } else {
+        try {
+            const userID = sqlstring.escape(uid);
+            const query  = `SELECT role FROM Profile WHERE userID = ${userID}`;
+            const result = await DB.runQuery(query);
+            if (result.length === 1 && result[0].role === ROLE_OPTIONS.ADMIN) {
+                res.send(true);
+            } else {
+                res.send(false);
+            }
+        } catch (e) {
+            console.log('Error retrieving role from database:', e);
+            res.send(false);
+        }
+    }
+});
+
+router.post('/updateRole', async (req, res) => {
+    const {uid, newRole} = req.body;
+    if (!uid || !newRole) {
+        console.log(`Unable to update profile's role because uid or newRole is undefined.`);
+        return res.status(500).json({
+                                        error: `Cannot update role.`,
+                                    });
+    } else {
+        try {
+            const userID = sqlstring.escape(uid);
+            const role   = sqlstring.escape(newRole);
+            // Check that it's a valid role
+            if (ROLE_OPTIONS_VALUES.indexOf(role) > -1) {
+                const query  = `UPDATE Profile SET role = ${role} WHERE userID = ${userID};`;
+                const result = await DB.runQuery(query);
+                res.json({success: true});
+            } else {
+                throw 'Invalid role';
+            }
+        } catch (e) {
+            console.log(`Unable to update profile's role:`, e);
+            return res.status(500).json({
+                                            error: `Cannot update role.`,
+                                        });
+        }
+    }
+});
+
+router.post('/getAllProfiles', async (req, res) => {
+    try {
+        const query  = `SELECT * FROM Profile;`;
+        const result = await DB.runQuery(query);
+        res.json(result);
+    } catch (e) {
+        console.log('Error retrieving all profiles from the database:', e);
+        res.status(500).json({
+                                 error: `Cannot retrieve profiles from the database.`,
+                             });
+    }
+
 });
 
 module.exports = router;
