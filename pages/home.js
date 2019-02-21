86a662e4629ab72ba4f735fdd4fb157e55055fd0
Huong Dang <huongd97@knights.ucf.edu>
2019-02-21 10:24:52 -0500

    feat: Add button to go to user accounts if the user is an admin

--- pages/home.js
+++ pages/home.js
@@ -1,11 +1,34 @@
 import axios from 'axios';
 import Button from '@material-ui/core/Button';
-import {signOut} from '../services/accounts';
-import Router from 'next/router'
+import {signOut, getCurrentUser} from '../services/accounts';
+import Router from 'next/router';
+import Loading from '../components/Loading';
+import isNil from 'lodash/isNil';
+import Link from 'next/link';
 
 class Home extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            loading: true
+        }
+    }
+
+    async componentDidMount() {
+        try {
+            if (!isNil(localStorage.uid)) {
+                const user = await getCurrentUser();
+                if (user.uid === localStorage.uid) {
+                    this.setState({loading: false})
+                } else {
+                    await signOut();
+                    localStorage.clear();
+                    Router.push('/');
+                }
+            }
+        } catch (e) {
+            console.log('e', e);
+        }
     }
 
     handleSubmit = async () => {
@@ -15,19 +38,32 @@ class Home extends React.Component {
         } catch (e) {
             console.log('Error signing out.', e);
         }
+    };
+
+    renderHomePage() {
+        if (this.state.loading) {
+            return <Loading/>;
+        } else {
+            return (
+                <div>
+                    <div>
+                        You're logged in!
+                    </div>
+                    <Link href={'/userAccounts'}>
+                        <Button>
+                            user accounts
+                        </Button>
+                    </Link>
+                    <Button color={'secondary'} variant={'outlined'} onClick={this.handleSubmit}>
+                        Sign Out
+                    </Button>
+                </div>
+            )
+        }
     }
 
     render() {
-        return (
-            <div>
-                <div>
-                    You're logged in!
-                </div>
-                <Button color={'secondary'} variant={'outlined'} onClick={this.handleSubmit}>
-                    Sign Out
-                </Button>
-            </div>
-        )
+        return this.renderHomePage();
     }
 }
 
