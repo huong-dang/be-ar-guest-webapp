f83fe3affada164e352bae8fffa31fe81dc17b0c
Huong Dang <huongd97@knights.ucf.edu>
2019-03-28 14:55:21 -0400

    feat: Add a review route is set up

--- app/model/Item.js
+++ app/model/Item.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var router  = express.Router();
-var DB      = require('../db');
-var _       = require('lodash');
+var express   = require('express');
+var router    = express.Router();
+var DB        = require('../db');
+var _         = require('lodash');
 var sqlstring = require('sqlstring');
 
 
@@ -14,6 +14,47 @@ router.post('/getAll', async (req, res) => {
     } catch (e) {
         console.log('Error', e);
         res.status(500).send('Something went wrong in the server.');
+    }
+});
+
+/**
+ * Input: {
+ *  "userID": string, // REQUIRED
+ *  "itemID": number, // REQUIRED
+ *  "comment": string,
+ *  "rating": number, // MUST BE BETWEEN 1 AND 5
+ *  "isFavorite": boolean,
+ *  "flag": boolean,
+ * }
+ *
+ * Output: {
+ *  "success": true
+ * }
+ *
+ * OR
+ *
+ * 'Check server logs for more info'
+ */
+router.post('/addReview', async (req, res) => {
+    try {
+        const {userID, itemID, comment, rating, isFavorite, flag} = req.body;
+        if (!_.isNil(rating) && Number.isInteger(rating) && (rating < 1 || rating > 5)) {
+            throw new Error('Rating must be between 1 and 5.');
+        }
+
+        const query = `insert into Review (userID, itemID, comment, rating, isFavorite, flag)
+                        values (${sqlstring.escape(userID)}, 
+                        ${sqlstring.escape(itemID)}, 
+                        ${sqlstring.escape(comment)}, 
+                        ${sqlstring.escape(rating)}, 
+                        ${sqlstring.escape((_.isNil(isFavorite) ? false : isFavorite))}, 
+                        ${sqlstring.escape((_.isNil(flag) ? false : isFavorite))});`
+
+        const result = await DB.runQuery(query);
+        res.json({success: true});
+    } catch (e) {
+        console.log('Error adding a review', e);
+        res.status(500).send('Check server logs for more info');
     }
 });
 
