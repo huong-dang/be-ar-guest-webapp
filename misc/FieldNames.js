cc68af6d3e2993ee8a7f61cc7533e1ba56aa8a45
Huong Dang <huongd97@knights.ucf.edu>
2019-04-01 00:22:39 -0400

    feat: Add updatable fields for restaurants

--- misc/FieldNames.js
+++ misc/FieldNames.js
@@ -1,1 +1,3 @@
 exports.ITEMS_UPDATABALE_FIELDS = ['itemName', 'itemDescription', 'secret', 'vegan', 'substitution', 'itemStatus', 'x', 'z'];
+
+exports.RESTAURANTS_UPDATABLE_FIELDS = ['restaurantName', 'restaurantTypeID', 'restaurantStatus', 'landID'];
