cc51fc7840f99a3a2145dd8f9b1dc880e75865ca
Huong Dang <huongd97@knights.ucf.edu>
2019-04-03 23:25:39 -0400

    feat: Make default dashboard drawer closed

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -107,7 +107,7 @@ class Dashboard extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            open:             true,
+            open:             false,
             selectedMenuItem: "Item"
         };
     }
