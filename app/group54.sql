ad534ff60ed9d7c273c47406ef91d2311cfc3de6
Huong Dang <huongd97@knights.ucf.edu>
2019-04-03 21:08:43 -0400

    feat: Add trip name as a field for TripPlan

--- app/group54.sql
+++ app/group54.sql
@@ -58,6 +58,7 @@ CREATE TABLE `TripPlan` (
   `userID` VARCHAR(100) NOT NULL,
   `startDate` DATETIME NOT NULL,
   `endDate` DATETIME NOT NULL,
+  `tripName` VARCHAR(200) NOT NULL,
   PRIMARY KEY (`tripID`),
   FOREIGN KEY (`userID`) REFERENCES `Profile` (`userID`)
 );
