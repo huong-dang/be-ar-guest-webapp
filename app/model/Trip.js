e71223ad245480ab1a98c152f14c7089440e97db
Huong Dang <huongd97@knights.ucf.edu>
2019-04-06 19:08:42 -0400

    feat: Add trip route

--- app/model/Trip.js
+++ app/model/Trip.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router  = express.Router();
-var DB      = require('../db');
-var _       = require('lodash');
+var express   = require('express');
+var router    = express.Router();
+var DB        = require('../db');
+var _         = require('lodash');
+var sqlstring = require('sqlstring');
 
 
-// TODO: Code here
+// Get all trips for a user
+router.post('/getByUserID', async (req, res) => {
+    const {userID} = req.body;
+    try {
+
+    } catch (e) {
+
+    }
+});
 
 module.exports = router;
