e7933b65f5f9fba0113745da17c9b73ed30d329c
jacquelynlaw <jacquelynlaw@gmail.com>
2019-04-09 14:00:29 -0400

    Item Dialog Information in progress, styling for dialog title, item description, item alterations, and buttons added. Missiong button functionality, ratings, and comments.

--- components/ItemCard.js
+++ components/ItemCard.js
@@ -5,12 +5,18 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import Grid from '@material-ui/core/Grid';
 import FavoriteIcon from '@material-ui/icons/FavoriteBorderOutlined';
+import TripIcon from '@material-ui/icons/DateRangeOutlined';
+import FlagIcon from '@material-ui/icons/OutlinedFlag';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
 
-const styles = {
+const styles = theme => ({
     card:         {
         width:        250,
         height:       250,
@@ -27,6 +33,7 @@ const styles = {
     },
     description:  {
         fontSize:   12.5,
+        fontFamily: 'Avenir',
         textAlign:  'center',
         lineHeight: 1.4,
         marginTop:  5,
@@ -60,48 +67,137 @@ const styles = {
         fontFamily: 'Avenir',
         fontSize: 12.5,
     },
-};
+    // Dialog Styles
+    dialogItemTitle: {
+        fontSize:   16,
+        fontWeight: '400',
+        lineHeight: 1.2,
+        textAlign: 'left', 
+        fontFamily: 'Avenir',
+        textTransform: 'uppercase',
+        [theme.breakpoints.between('xs', 'md')]: {
+            paddingTop: 20,
+        },
+    },
+    dialogFavoriteButton: {
+        position: 'absolute',
+        right: 15,
+    },
+    addToTripButton: {
+        position: 'absolute',
+        // top: 15,
+        right: 45,
+    },
+    flagButton: {
+        position: 'absolute',
+        // top: 15,
+        right: 75,
+    },
+    dialogDescription: {
+        fontSize:   12.5,
+        fontFamily: 'Avenir',
+        textAlign:  'center',
+        lineHeight: 1.4,
+    },
+});
 
-function ItemCard(props) {
-    const {classes} = props;
+class ItemCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false,
+        }
+    }
 
-    return (
-        <Card className={classes.card}>
-            <CardContent>
-                <Grid container direction="row" justify="space-between" alignItems="center">
-                    <Grid container direction="row" alignItems="center">
-                        <Grid item xs>
-                            <Typography className={classes.title}>
-                                {props.itemName}
-                            </Typography>
-                        </Grid>
-                        <Grid item>
-                            <FavoriteIcon className={classes.favoriteIcon}/>
+    handleOpen = () => {
+        this.setState({ open: true });
+    }
+    
+    handleClose = () => {
+        this.setState({ open: false });
+    }
+
+    render () {
+        const { classes, itemName, itemDescription, substitution } = this.props;
+
+        return (
+            <div>
+            <Card className={classes.card}>
+                <CardContent>
+                    <Grid container direction="row" justify="space-between" alignItems="center">
+                        <Grid container direction="row" alignItems="center">
+                            <Grid item xs>
+                                <Typography className={classes.title}>
+                                    {itemName}
+                                </Typography>
+                            </Grid>
+                            <Grid item>
+                                <FavoriteIcon className={classes.favoriteIcon}/>
+                            </Grid>
                         </Grid>
                     </Grid>
-                </Grid>
-                <Typography className={classes.description} color="textSecondary">
-                    {/* Plant-based sausage topped with pickled slaw, BBQ vegan aioli, & roasted corn relish. */}
-                    {props.itemDescription}
-                </Typography>
-                {/* <Typography className={classes.description} color="textSecondary">
-                    Served with french fries or apple slices.
-                </Typography> */}
-                <Divider variant="middle" style={{marginTop: 8}}/>
-                <Grid container direction="row" justify="flex-start" alignItems="flex-start">
-                    <Typography className={classes.alterations} color="textSecondary">
-                        Alterations:
+                    <Typography className={classes.description} color="textSecondary">
+                        {/* Plant-based sausage topped with pickled slaw, BBQ vegan aioli, & roasted corn relish. */}
+                        {itemDescription}
                     </Typography>
-                    <Typography className={classes.alterationsDescription} color="textSecondary">
-                        {props.substitution}
-                    </Typography>
-                </Grid>
-                <Button variant="outlined" className={classes.expandButton}>
-                    <Typography className={classes.expandButtonText}>View More</Typography>
-                </Button>
-            </CardContent>
-        </Card>
-    );
+                    {/* <Typography className={classes.description} color="textSecondary">
+                        Served with french fries or apple slices.
+                    </Typography> */}
+                    <Divider variant="middle" style={{marginTop: 8}}/>
+                    <Grid container direction="row" justify="flex-start" alignItems="flex-start">
+                        <Typography className={classes.alterations} color="textSecondary">
+                            Alterations:
+                        </Typography>
+                        <Typography className={classes.alterationsDescription} color="textSecondary">
+                            {substitution}
+                        </Typography>
+                    </Grid>
+                    <Button variant="outlined" className={classes.expandButton} onClick={this.handleOpen}>
+                        <Typography className={classes.expandButtonText}>View More</Typography>
+                    </Button>
+                </CardContent>
+            </Card>
+            <Dialog
+                open={this.state.open}
+                onClose={this.handleClose}
+                aria-labelledby="form-dialog-title"
+            >
+                <DialogTitle className={classes.dialogItemTitle}>
+                    {itemName}
+                    <IconButton className={classes.dialogFavoriteButton}>
+                        <FavoriteIcon />
+                    </IconButton>
+                    <IconButton className={classes.addToTripButton}>
+                        <TripIcon />
+                    </IconButton>
+                    <IconButton className={classes.flagButton}>
+                        <FlagIcon />
+                    </IconButton>
+                </DialogTitle>
+                <DialogContent>
+                    <Grid container spacing={24}>
+                        <Grid item xs={12} md={6}>
+                        <DialogContent className={classes.dialogDescription}>
+                            <Typography variant="overline">Description: <br/></Typography>
+                            {itemDescription}
+                            <br />
+                            <Typography variant="overline">Alterations: <br/></Typography>
+                            {substitution}
+                        </DialogContent>
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                        <DialogContent>
+                            <Card>
+                                comments
+                            </Card>
+                        </DialogContent>
+                        </Grid>
+                    </Grid>
+                </DialogContent>
+            </Dialog>
+            </div>
+        );
+    }
 }
 
 ItemCard.propTypes = {
