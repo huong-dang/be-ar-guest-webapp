c4618ac68ad922beea9cdf8b519c23c8c9fd8a99
jacquelynlaw <jacquelynlaw@gmail.com>
2019-04-17 13:28:40 -0400

    Account page functionality finished. User can request a reset password email and view a list of their favorited items.

--- components/Account.js
+++ components/Account.js
@@ -8,6 +8,13 @@ import Grid from '@material-ui/core/Grid';
 import FormControl from '@material-ui/core/FormControl';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import {getCurrentUser, resetPassword} from '../services/accounts';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import axios from 'axios';
+import FavoriteItem from './FavoriteItem';
 
 const styles = theme => ({
     root:           {
@@ -63,7 +70,7 @@ const styles = theme => ({
     },
     changePasswordButton: {
         marginLeft: 10,
-        marginTop: 35,
+        // marginTop: 35,
         marginBottom: 10,
     },
 });
@@ -72,12 +79,81 @@ class Account extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            user: props.user
+            user: props.user,
+            currentPassword: "",
+            newPassword: "",
+            newPasswordVerify: "",
+            passwordsMatch: true,
+            passwordChangeSuccess: false,
+            favorites: [],
+            passwordChangeSuccess: false,
         }
     }
 
+    async componentDidMount() {
+        try {
+            const result = await axios.post('/review/getUserFavoritedItems', {userID: this.state.user.userID});
+            console.log("Account.js result =>", result.data);
+            this.setState({
+                              favorites: result.data,
+                          });
+        } catch (e) {
+            console.log('Error', e);
+        }
+    }
+
+    handleChange = prop => event => {
+        this.setState({ [prop]: event.target.value });
+    };
+
+    handlePasswordReset = async () => {
+        // if (this.state.newPassword === this.state.newPasswordVerify) {
+        //     const result = await resetPassword(this.state.newPassword);
+        //     if (result.success) {
+        //         this.setState({ passwordChangeSuccess: true });
+        //     } else {
+        //         // Password was not successfully updated
+        //         console.log('Error: Password not updated successfully.');
+        //     } 
+        //     console.log("Value of state.newPassword =>", this.state.newPassword);
+        //     console.log("Value of newPasswordVerify => ", this.state.newPasswordVerify);
+            
+        // } else{
+        //     console.log("Passwords do not match");
+        //     this.setState({ passwordsMatch: false });
+        // }
+        const emailSent = await resetPassword(this.state.user.email);
+        console.log('ResetPassword emailSent =>', emailSent);
+        // if (emailSent.success) {
+        //     this.setState({ passwordChangeSuccess: true });
+        // } else {
+        //     // Password was not successfully updated
+        //     console.log('Error: Reset password email not sent');
+        // }
+    };
+
+    renderDoNotMatch() {
+        return (
+            <div>
+                <Typography variant="overline">
+                    Passwords do not match.
+                </Typography>
+            </div>
+        );
+    };
+
     render() {
         const { classes } = this.props;
+        const favoritesInfo = this.state.favorites.map((favoriteItem, index) => {
+            return (
+                <Grid item xs={12} sm={4} key={favoriteItem.itemID}>
+                    <FavoriteItem 
+                        item={favoriteItem}
+                        user={this.state.user}
+                    />
+                </Grid>
+            );
+        });
         return (
             <div style={{ width: '80%',}}>
                 {/* Account page: {this.state.user.fName ? this.state.user.fName : 'no name'} */}
@@ -158,40 +234,7 @@ class Account extends React.Component {
                         Change Password
                     </Typography>
                     <Divider variant="middle"/>
-                    <Grid container spacing={24}>
-                        <Grid item>
-                        <Grid container
-                            direction="column"
-                            justify="flex-start"
-                            alignItems="flex-start"
-                            style={{
-                                paddingTop: 10,
-                                marginLeft: 10,
-                            }}
-                        >
-                            <Grid item className={classes.emailGrid}>
-                                <Typography className={classes.passwordTitle}>
-                                    Current Password*
-                                </Typography>
-                            </Grid>
-                            <Grid item>
-                                <form className={classes.passwordTextField}>
-                                    <FormControl
-                                        style={{
-                                            height: 30,
-                                        }}
-                                    >
-                                        <TextField
-                                            id="currentPassword"
-                                            variant="outlined"
-                                            required
-                                            autoFocus={false}
-                                        />
-                                    </FormControl>
-                                </form>
-                            </Grid>
-                        </Grid>
-                        </Grid>
+                    {/* <Grid container spacing={24}>
                         <Grid item>
                         <Grid container
                             direction="column"
@@ -220,7 +263,8 @@ class Account extends React.Component {
                                             required
                                             fullWidth
                                             autoFocus={false}
-                                            
+                                            value={this.state.newPassword}
+                                            onChange={this.handleChange('newPassword')}
                                         />
                                     </FormControl>
                                 </form>
@@ -255,22 +299,44 @@ class Account extends React.Component {
                                             required
                                             fullWidth
                                             autoFocus={false}
-                                            
+                                            value={this.state.newPasswordVerify}
+                                            onChange={this.handleChange('newPasswordVerify')}
                                         />
                                     </FormControl>
                                 </form>
                             </Grid>
                         </Grid>
                         </Grid>
-                    </Grid>
+                    </Grid> */}
+                    {/* {this.state.passwordsMatch ? <div></div> : this.renderDoNotMatch() } */}
+                    <Typography variant="overline"
+                        style={{
+                            marginLeft: 10,
+                        }}
+                    >
+                        Send a reset password email
+                    </Typography>
                     <Button 
                         size="medium" 
                         variant="outlined"
                         className={classes.changePasswordButton}
+                        onClick={this.handlePasswordReset}
                     >
                         Change Password
                     </Button>
                 </Card>
+                <ExpansionPanel>
+                    <ExpansionPanelSummary expandIcon={<ExpandMoreIcon/>}>
+                        <Typography className={classes.h1}>
+                            Favorites
+                        </Typography>
+                    </ExpansionPanelSummary>
+                    <ExpansionPanelDetails>
+                        <Grid container spacing={24}>
+                            {favoritesInfo}
+                        </Grid>
+                    </ExpansionPanelDetails>
+                </ExpansionPanel>
             </div>
         )
     }
